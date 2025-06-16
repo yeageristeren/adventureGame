ae03ce1e95094df64945c4ab68c2c957cdb3e0f9
mohit <mohitsharma89890@gmail.com>
2025-06-16 12:26:55 +0530

    Room class initialised

--- src/com/textadventure/Model/Item.java
+++ src/com/textadventure/Model/Item.java
@@ -1,18 +1,24 @@
 package com.textadventure.Model;
 
 public class Item {
-    private String itemName;
-    private String itemDescription;
+    private String name;
+    private String description;
 
     Item(String itemName, String itemDescription) {
-        this.itemName = itemName;
-        this.itemDescription = itemDescription;
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Item name cannot be null or empty.");
+        }
+        if (description == null) {
+            throw new IllegalArgumentException("Item description cannot be null.");
+        }
+        this.name = itemName;
+        this.description = itemDescription;
     }
 
     public String getItemName() {
-        return itemName;
+        return name;
     }
     public String getItemDescription() {
-        return itemDescription;
+        return description;
     }
 }
--- src/com/textadventure/Model/Room.java
+++ src/com/textadventure/Model/Room.java
@@ -1,4 +1,56 @@
 package com.textadventure.Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Room {
+    private String name;
+    private String description;
+    private HashMap<String, String> exits = new HashMap<>();
+    private ArrayList<Item> items = new ArrayList<>();
+
+    Room(String name, String description, HashMap<String, String> exit, ArrayList<Item> items) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Room name cannot be null or empty.");
+        }
+        if (description == null) {
+            throw new IllegalArgumentException("Room description cannot be null.");
+        }
+
+        this.name = name.trim();
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public HashMap<String, String> getExits() {
+        return this.exits;
+    }
+    public ArrayList<Item> getItems() {
+        return this.items;
+    }
+
+    public void addItem(Item item){
+        if(item==null){
+            throw new IllegalArgumentException("Invalid item: NULL");
+        }
+        items.add(item);
+    }
+    public boolean removeItem(Item item){
+        if(item==null){
+            throw new IllegalArgumentException("Invalid item: NULL");
+        }
+        return items.remove(item);//returns whether the item is removed
+    }
+    public void addExit(String direction,String roomName){
+        if(roomName==null||direction==null){
+            throw new IllegalArgumentException("Invalid item: NULL");
+        }
+        exits.put(direction.trim().toUpperCase(),roomName.trim());
+    }
 }
